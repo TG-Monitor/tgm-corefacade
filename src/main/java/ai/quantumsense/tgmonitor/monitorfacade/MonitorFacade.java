d16cc759ec218032a15db9346a83c7123cba19ab
Daniel Weibel <danielmweibel@gmail.com>
2018-07-20 18:51:41 +0800

    Add MonitorFacade interface and standard implementation

--- src/main/java/ai/quantumsense/tgmonitor/monitorfacade/MonitorFacade.java
+++ src/main/java/ai/quantumsense/tgmonitor/monitorfacade/MonitorFacade.java
@@ -1,4 +1,65 @@
 package ai.quantumsense.tgmonitor.monitorfacade;
 
-public interface MonitorFacade {
+import ai.quantumsense.tgmonitor.entities.Emails;
+import ai.quantumsense.tgmonitor.entities.Patterns;
+import ai.quantumsense.tgmonitor.entities.Peers;
+import ai.quantumsense.tgmonitor.monitor.Monitor;
+
+import java.util.Set;
+
+public interface MonitorFacade extends Monitor, Peers, Patterns, Emails {
+
+    @Override
+    void login(String phoneNumber);
+    @Override
+    void logout();
+    @Override
+    boolean isLoggedIn();
+    @Override
+    void start();
+    @Override
+    void stop();
+    @Override
+    boolean isRunning();
+    @Override
+    String getPhoneNumber();
+
+    @Override
+    Set<String> getPeers();
+    @Override
+    void setPeers(Set<String> peers);
+    @Override
+    void addPeer(String peer);
+    @Override
+    void addPeers(Set<String> peers);
+    @Override
+    void removePeer(String peer);
+    @Override
+    void removePeers(Set<String> peers);
+
+    @Override
+    Set<String> getPatterns();
+    @Override
+    void setPatterns(Set<String> patterns);
+    @Override
+    void addPattern(String pattern);
+    @Override
+    void addPatterns(Set<String> patterns);
+    @Override
+    void removePattern(String pattern);
+    @Override
+    void removePatterns(Set<String> patterns);
+
+    @Override
+    Set<String> getEmails();
+    @Override
+    void setEmails(Set<String> emails);
+    @Override
+    void addEmail(String email);
+    @Override
+    void addEmails(Set<String> emails);
+    @Override
+    void removeEmail(String email);
+    @Override
+    void removeEmails(Set<String> emails);
 }
---
+++ src/main/java/ai/quantumsense/tgmonitor/monitorfacade/MonitorFacadeImpl.java
@@ -0,0 +1,152 @@
+package ai.quantumsense.tgmonitor.monitorfacade;
+
+import ai.quantumsense.tgmonitor.entities.Emails;
+import ai.quantumsense.tgmonitor.entities.Patterns;
+import ai.quantumsense.tgmonitor.entities.Peers;
+import ai.quantumsense.tgmonitor.monitor.Monitor;
+import ai.quantumsense.tgmonitor.servicelocator.ServiceLocator;
+
+import java.util.Set;
+
+public class MonitorFacadeImpl implements MonitorFacade {
+
+    private ServiceLocator<Monitor> monitorLocator;
+    private ServiceLocator<Peers> peersLocator;
+    private ServiceLocator<Patterns> patternsLocator;
+    private ServiceLocator<Emails> emailsLocator;
+
+    public MonitorFacadeImpl(ServiceLocator<Monitor> monitorLocator,
+                             ServiceLocator<Peers> peersLocator,
+                             ServiceLocator<Patterns> patternsLocator,
+                             ServiceLocator<Emails> emailsLocator) {
+        this.monitorLocator = monitorLocator;
+        this.peersLocator = peersLocator;
+        this.patternsLocator = patternsLocator;
+        this.emailsLocator = emailsLocator;
+    }
+
+    @Override
+    public void login(String phoneNumber) {
+        monitorLocator.getService().login(phoneNumber);
+    }
+
+    @Override
+    public void logout() {
+        monitorLocator.getService().logout();
+    }
+
+    @Override
+    public boolean isLoggedIn() {
+        return monitorLocator.getService().isLoggedIn();
+    }
+
+    @Override
+    public void start() {
+        monitorLocator.getService().start();
+    }
+
+    @Override
+    public void stop() {
+        monitorLocator.getService().stop();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return monitorLocator.getService().isRunning();
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return monitorLocator.getService().getPhoneNumber();
+    }
+
+    @Override
+    public Set<String> getPeers() {
+        return peersLocator.getService().getPeers();
+    }
+
+    @Override
+    public void setPeers(Set<String> peers) {
+        peersLocator.getService().setPeers(peers);
+    }
+
+    @Override
+    public void addPeer(String peer) {
+        peersLocator.getService().addPeer(peer);
+    }
+
+    @Override
+    public void addPeers(Set<String> peers) {
+        peersLocator.getService().addPeers(peers);
+    }
+
+    @Override
+    public void removePeer(String peer) {
+        peersLocator.getService().removePeer(peer);
+    }
+
+    @Override
+    public void removePeers(Set<String> peers) {
+        peersLocator.getService().removePeers(peers);
+    }
+
+    @Override
+    public Set<String> getPatterns() {
+        return patternsLocator.getService().getPatterns();
+    }
+
+    @Override
+    public void setPatterns(Set<String> patterns) {
+        patternsLocator.getService().setPatterns(patterns);
+    }
+
+    @Override
+    public void addPattern(String pattern) {
+        patternsLocator.getService().addPattern(pattern);
+    }
+
+    @Override
+    public void addPatterns(Set<String> patterns) {
+        patternsLocator.getService().addPatterns(patterns);
+    }
+
+    @Override
+    public void removePattern(String pattern) {
+        patternsLocator.getService().removePattern(pattern);
+    }
+
+    @Override
+    public void removePatterns(Set<String> patterns) {
+        patternsLocator.getService().addPatterns(patterns);
+    }
+
+    @Override
+    public Set<String> getEmails() {
+        return emailsLocator.getService().getEmails();
+    }
+
+    @Override
+    public void setEmails(Set<String> emails) {
+        emailsLocator.getService().setEmails(emails);
+    }
+
+    @Override
+    public void addEmail(String email) {
+        emailsLocator.getService().addEmail(email);
+    }
+
+    @Override
+    public void addEmails(Set<String> emails) {
+        emailsLocator.getService().setEmails(emails);
+    }
+
+    @Override
+    public void removeEmail(String email) {
+        emailsLocator.getService().removeEmail(email);
+    }
+
+    @Override
+    public void removeEmails(Set<String> emails) {
+        emailsLocator.getService().removeEmails(emails);
+    }
+}
